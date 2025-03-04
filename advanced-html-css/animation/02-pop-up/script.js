d4ccdce0d015de3ca5a46a6dd40632ba3c56e7bd
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-03-04 13:12:07 -0600

    Complete translate popup

--- advanced-html-css/animation/02-pop-up/script.js
+++ advanced-html-css/animation/02-pop-up/script.js
@@ -2,10 +2,10 @@ const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
 
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
-  modalDiv.classList.toggle('show');
-  backdrop.classList.toggle('show');
+    const modalDiv = document.querySelector('.popup-modal');
+    const backdrop = document.querySelector('.backdrop');
+    modalDiv.classList.toggle('show');
+    backdrop.classList.toggle('show');
 }
 
 openButton.addEventListener('click', toggleModal);
